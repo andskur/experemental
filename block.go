11d177da29266297366a84f0c0bedd0522e40d00
Andrew Skurlatov <a.skurlatov@gmail.com>
2018-12-09 19:59:32 +0300

    Basic prototype

---
+++ block.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+// Block keeps block headers
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+// SetHash calculates and sets block hash
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	//fmt.Printf("Block timestamp: %s\n", timestamp)
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+// NewBlock creates and returns Block
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().UnixNano(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+
+	// Log info
+	//fmt.Println(block.Data)
+	return block
+}
+
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	bc := NewBlockchain()
+
+
+	bc.AddBlock("Remittance tx from Moscow to Dushanbe")
+	bc.AddBlock("Remittance tx from Dushanbe to Tashkent")
+
+	//fmt.Println(bc.blocks[0].Data)
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
